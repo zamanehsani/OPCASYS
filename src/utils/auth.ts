77c99c50664f0146aa9daa0257d32662d30064b8
Zaman Ehsani <zamanehsani@gmail.com>
2024-08-17 14:32:10 +0400

    made some changes in auth

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -9,60 +9,9 @@ import { PrismaClient } from "@prisma/client";
 import { prisma } from "./prismaDB";
 import type { Adapter } from "next-auth/adapters";
 
-export const authOptions: NextAuthOptions = {
-  pages: {
-    signIn: "/auth/signin",
-  },
-  adapter: PrismaAdapter(prisma) as Adapter,
-  secret: process.env.SECRET,
-  session: {
-    strategy: "jwt",
-  },
-
+export const authOptions: any = {
+  pages: { signIn: "/auth/signin" },
   providers: [
-    CredentialsProvider({
-      name: "credentials",
-      credentials: {
-        email: { label: "Email", type: "text", placeholder: "Jhondoe" },
-        password: { label: "Password", type: "password" },
-        username: { label: "Username", type: "text", placeholder: "Jhon Doe" },
-      },
-
-      async authorize(credentials) {
-        // check to see if email and password is there
-        if (!credentials?.email || !credentials?.password) {
-          throw new Error("Please enter an email or password");
-        }
-
-        // check to see if user already exist
-        const user = await prisma.user.findUnique({
-          where: {
-            email: credentials.email,
-          },
-        });
-
-        // if user was not found
-        if (!user || !user?.password) {
-          throw new Error("In valid email or password");
-        }
-
-        // check to see if passwords match
-        const passwordMatch = await bcrypt.compare(
-          credentials.password,
-          user.password,
-        );
-
-        // console.log(passwordMatch);
-
-        if (!passwordMatch) {
-          console.log("test", passwordMatch);
-          throw new Error("Incorrect password");
-        }
-
-        return user;
-      },
-    }),
-
     GitHubProvider({
       clientId: process.env.GITHUB_CLIENT_ID!,
       clientSecret: process.env.GITHUB_CLIENT_SECRET!,
@@ -98,19 +47,6 @@ export const authOptions: NextAuthOptions = {
         };
       }
       return token;
-    },
-
-    session: async ({ session, token }) => {
-      if (session?.user) {
-        return {
-          ...session,
-          user: {
-            ...session.user,
-            id: token?.id,
-          },
-        };
-      }
-      return session;
     },
   },
 
